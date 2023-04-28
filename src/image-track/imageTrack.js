e153522b28b640f481dd63e87c1a2746104be3ac
Chef <kvu6111@gmail.com>
2023-04-28 16:50:12 -0600

    new portfolio for me

---
+++ src/image-track/imageTrack.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, useRef } from'react';
+import { motion } from "framer-motion";
+import images from './images';
+
+const ImageTrack = () => {
+
+    // Carousel movement
+    const [width, setWidth] = useState(0);
+    const carousel = useRef();
+    useEffect(() => {
+        setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }, [])
+
+    return (
+        <div>
+            <motion.div ref={carousel} id="image-track" className="home-page">
+            <motion.div 
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            id="inner-image-track"
+            >
+                {images.map(image => {
+                return (
+                    <motion.div id='image-container'>
+                    <img src={image} alt="" key="" draggable="false" />
+                    </motion.div>
+                    );
+                })}
+            </motion.div>
+            </motion.div>
+        </div>
+        
+    )
+}
+
+export default ImageTrack;
---
+++ src/navbar/navbar.js
@@ -0,0 +1,17 @@
+// Imports
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className = 'navbar'>
+            <div className = 'navbar-name'> 
+                <h1 className = 'navbar-name'> Khoi Vu  </h1>
+                <h3 className = 'navbar-name-title'> / Full Stack Web Developer </h3>
+            </div>
+        </div>
+    )
+};
+
+// Export
+export default Navbar;
